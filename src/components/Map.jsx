3e73a1a9880069cf70f5e9b27bcc5e94c2e265a3
Bence Hornák <hornak.bence@gmail.com>
2019-10-26 17:36:36 +0200

    Google Maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
-function Map() {
-    return (
-        <p>Map</p>
-    );
-}
+import config from '../config.json';
 
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+} from "react-google-maps";
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+    <GoogleMap
+        defaultZoom={14}
+        defaultCenter={{ lat: 60.166557, lng: 24.931950 }}
+    >
+        <Marker
+            position={{ lat: 60.166557, lng: 24.931950 }}
+        />
+    </GoogleMap>
+));
+
+function Map() {
+    return <MapWithAMarker
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config.googleMapsKey}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+    />;
+}
 
 export default Map;
