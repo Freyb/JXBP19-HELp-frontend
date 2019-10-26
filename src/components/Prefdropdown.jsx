edacab4e6a4137e2e6f489ff23e2983b0d40d12e
Freyb <balazs.frey@gmail.com>
2019-10-26 17:55:25 +0200

    Implement Dropdown

--- src/components/Prefdropdown.jsx
+++ src/components/Prefdropdown.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import "./prefdropdown.css";
+import data from "../tags_list";
 
 function Prefdropdown() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
+  const select = event => console.log(event.currentTarget.value);
+
   return (
     <div>
-      <p>Form</p>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>Dropdown</DropdownToggle>
+        <DropdownMenu className={"mydropdown"}>
+          {[...data].sort().map(d => (
+            <DropdownItem key={d} value={d} onClick={select}>
+              {d}
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 }
