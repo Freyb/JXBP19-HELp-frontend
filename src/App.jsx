150fe5f02a926de5348fe20d1f7d3d3823e7dc30
Bence Hornák <hornak.bence@gmail.com>
2019-10-26 17:05:20 +0200

    Reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'reactstrap';
+import Map from './components/Map';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col md="3">
+          <p>Form</p>
+        </Col>
+        <Col md="9">
+          <Map />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
