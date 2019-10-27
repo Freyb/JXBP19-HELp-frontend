7776398d5090feb7acdf47b31ccd953d99e6e0fb
Bence Hornák <hornak.bence@gmail.com>
2019-10-27 09:36:05 +0100

    Query places, pulse animation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,11 +5,12 @@ import {
     withScriptjs,
     withGoogleMap,
     GoogleMap,
+    Marker
 } from "react-google-maps";
 import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer';
 
 
-const MapWithAMarker = withScriptjs(withGoogleMap(({ heatmap }) => {
+const MapWithAMarker = withScriptjs(withGoogleMap(({ heatmap, onClick, marker }) => {
 
     const heatmapWithLatLon = heatmap.map(heatmapRaw => ({
         location: new window.google.maps.LatLng(heatmapRaw.latitude, heatmapRaw.longitude),
@@ -17,6 +18,7 @@ const MapWithAMarker = withScriptjs(withGoogleMap(({ heatmap }) => {
     }));
 
     return <GoogleMap
+        onClick={onClick}
         defaultZoom={14}
         options={{ minZoom: 14, maxZoom: 14, zoomControl: false }}
         defaultCenter={{ lat: 60.166557, lng: 24.931950 }}
@@ -26,16 +28,18 @@ const MapWithAMarker = withScriptjs(withGoogleMap(({ heatmap }) => {
             options={{
                 radius: 40,
             }} />
+
+        {marker && <Marker position={marker} />}
     </GoogleMap>
 }));
 
-function Map({ heatmap }) {
+function Map(props) {
     return <MapWithAMarker
         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config.googleMapsKey}&v=3.exp&libraries=geometry,drawing,places,visualization`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: '100vh' }} />}
         mapElement={<div style={{ height: `100% ` }} />}
-        heatmap={heatmap}
+        {...props}
     />;
 }
 
